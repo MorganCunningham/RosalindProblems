4ce12826d2ecb34ea13092cf309b348a784884a9
Morgan Cunningham <morganmcleodcunningham@gmail.com>
2024-09-19 12:44:22 -0700

    feat: transcribe rna function

---
+++ Transcribing_DNA/Transcirbing_DNA_to_RNA.py
@@ -0,0 +1,20 @@
+class RNA_String_object:
+    def __init__(self, string):
+        self.string = ""
+
+
+def Transcribe(DNA_String, RNA_String):
+    
+      
+    RNA_String.string = DNA_String.replace("T","U")
+
+    return RNA_String
+def main():
+
+    DNA_String_1 = open("Transcribing_DNA/rosalind_rna.txt").read()
+    RNA_String_1 = RNA_String_object("") 
+    RNA_String_1 = Transcribe(DNA_String_1, RNA_String_1)
+    print(RNA_String_1.string)
+
+if __name__ == '__main__':
+    main()
